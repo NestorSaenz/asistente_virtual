6f29386b6f95a74d108124650ddf5090f183e8af
Nestor Saenz <nes164@gmail.com>
2024-12-16 17:14:37 -0500

    flask

--- voice_Syncronized.py
+++ voice_Syncronized.py
@@ -1,48 +1,8 @@
 import time
 import azure.cognitiveservices.speech as speechsdk
-from openai_handler import get_response_from_model
-from azure_speech_handler import *
-from config import AZURE_SPEECH_KEY, AZURE_SPEECH_REGION
-
-import time
-import azure.cognitiveservices.speech as speechsdk
-from config import AZURE_SPEECH_KEY, AZURE_SPEECH_REGION
-
-# def display_and_speak(text, delay=0.03):
-#     """
-#     Muestra texto carácter por carácter y lo reproduce en voz a medida que se imprime.
-#     """
-#     # Configuración del sintetizador de voz
-#     speech_config = speechsdk.SpeechConfig(subscription=AZURE_SPEECH_KEY, region=AZURE_SPEECH_REGION)
-#     audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
-#     synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
-
-#     current_sentence = ""
-#     buffer = ""
-
-#     for char in text:
-#         print(char, end="", flush=True)  # Imprime el texto carácter por carácter
-#         buffer += char  # Acumula en un buffer temporal
-#         time.sleep(delay)  # Añade un retraso entre caracteres
-
-#         # Cuando encuentra un delimitador de frase, sintetiza el buffer y reinicia
-#         if char in ".!?":
-#             current_sentence += buffer
-#             synthesizer.speak_text_async(current_sentence).get()  # Reproduce la frase actual
-#             buffer = ""  # Reinicia el buffer
-#             current_sentence = ""  # Reinicia la construcción de la frase
-
-#     # Reproduce cualquier texto que haya quedado pendiente
-#     if buffer.strip():
-#         synthesizer.speak_text_async(buffer).get()
-
-#     print()  # Salto de línea final
-
-
-import time
 import threading
-import azure.cognitiveservices.speech as speechsdk
-from config import AZURE_SPEECH_KEY, AZURE_SPEECH_REGION
+from azure_speech_handler import *
+from config import *
 
 def text_to_speech_worker(text, synthesizer):
     """
@@ -65,9 +25,11 @@ def display_and_speak(text, delay = 0.08):
     """
     # Configuración del sintetizador de voz
     speech_config = speechsdk.SpeechConfig(subscription=AZURE_SPEECH_KEY, region=AZURE_SPEECH_REGION)
+    speech_config.speech_synthesis_voice_name = "es-CO-GonzaloNeural"  # Voz masculina colombiana
     audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
     synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
-
+    
+    
     # Crear hilos para la impresión y la síntesis de voz
     display_thread = threading.Thread(target=display_text_worker, args=(text, delay))
     speech_thread = threading.Thread(target=text_to_speech_worker, args=(text, synthesizer))
